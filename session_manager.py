a4c7d9f1231460143c4bacce0a1272712a2b2167
Sidney2408 <sidney_suen@mymail.sutd.edu.sg>
2018-07-14 09:11:57 +0800

    Updated the route to test JWT features

--- session_manager.py
+++ session_manager.py
@@ -1,3 +1,4 @@
+from flask_jwt_extended import jwt_required
 from flask import Blueprint, Flask, url_for
 from flask import jsonify
 import requests
@@ -51,8 +52,23 @@ def api_test2():
         print(traceback.print_tb(exc_traceback, file=sys.stdout))
         return jsonify(session_response)
 
+@TestURL.route('/auth_test', methods=['POST'])
+@jwt_required # JWT web decorator
+def api_test2():
+    try:
+        request_json = request.get_json(force=True)# You'll get a dictionary
+        print(type(request_json))
+        #request_json = json.dumps(request_json)
+        print("Request received: {}".format(request_json))
+        session_response = {"Success":"Success"}
+        return jsonify(session_response)
+
+    except Exception as ex:
+        print(ex)
+        return jsonify({"response": "error in processing sent request", "ex": str(ex)})
+    
 def setup():
-        return [SessionMGR]
+        return [SessionMGR,TestURL]
 
 
 
